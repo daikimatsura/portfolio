608da694b479ca8a3d2a9c4e6aaaad492f80d3c9
matsura <matsura@geekfeed.co.jp>
2025-02-27 21:09:01 +0900

    整理

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,9 +7,27 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: {
+    extends: ["eslint:recommended"],
+  },
 });
 
 const eslintConfig = [
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    files: ["**/*.ts", "**/*.tsx", "**/*.js", "**/*.jsx", "**/*.mjs"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
